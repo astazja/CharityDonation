1eb95382847183402f03ab168d116b7673984e80
Katarzyna Pacewicz <katarzyna@pacewi.cz>
2020-08-26 22:31:54 +0200

    Landing Page

--- donation/views.py
+++ donation/views.py
@@ -7,16 +7,17 @@ from .models import Institution, Donation
 class LandingPage(View):
     def get(self, request):
        bags = Donation.objects.all()
-       num_institutions = Institution.objects.count()
+       num_institutions = Donation.objects.values('institution_id').distinct().count()
+       institutions_list = Institution.objects.prefetch_related('categories__institution_set').all()
        sum_bag = 0
        for bag in bags:
            sum_bag += bag.quantity
        context = {
            'bags': sum_bag,
-           'institutions': num_institutions
+           'institutions': num_institutions,
+           'institutions_list':institutions_list
        }
        return render(request, "index.html", context)
-
 
 class AddDonation(View):
     def get(self, request):
